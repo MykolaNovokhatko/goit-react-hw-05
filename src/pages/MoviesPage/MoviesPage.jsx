99cd21f3cab7738eeb57799bf6edc7b407dd2d57
MykolaNovokhatko <143940513+MykolaNovokhatko@users.noreply.github.com>
2024-05-29 20:08:51 +0100

    hw5

--- src/pages/MoviesPage/MoviesPage.jsx
+++ src/pages/MoviesPage/MoviesPage.jsx
@@ -1,22 +1,42 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
+import { useSearchParams } from 'react-router-dom';
 import axios from 'axios';
 import MovieList from '/src/components/MovieList/MovieList';
-import css from './MoviesPage.module.css'
+import css from './MoviesPage.module.css';
 
 const MoviesPage = () => {
-  const [query, setQuery] = useState('');
   const [movies, setMovies] = useState([]);
+  const [searchParams, setSearchParams] = useSearchParams();
+  const query = searchParams.get('query') || '';
 
-  const handleSearch = async (event) => {
+  useEffect(() => {
+    const fetchMovies = async () => {
+      if (!query) return;
+
+      try {
+        const response = await axios.get('https://api.themoviedb.org/3/search/movie', {
+          params: { query, include_adult: false, language: 'en-US', page: 1 },
+          headers: {
+            Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJiNWJhOWZkMzIwNDVmNDA4NDdhZjFkYjBmOTBiMzcwNiIsInN1YiI6IjY2NTMyZTIzOWU1ZTFhMjhlNGM2MzFiOCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.1vuZ6E7VvQZie1MYaKOKJwVqh9fKIhy1Wk739Fe7cyU',
+          },
+        });
+        setMovies(response.data.results);
+      } catch (error) {
+        console.error('Error searching movies', error);
+      }
+    };
+
+    fetchMovies();
+  }, [query]);
+
+  const handleSearch = (event) => {
     event.preventDefault();
-    try {
-      const response = await axios.get(`https://api.themoviedb.org/3/search/movie`, {
-        params: { query, include_adult: false, language: 'en-US', page: 1 },
-        headers: { Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJiNWJhOWZkMzIwNDVmNDA4NDdhZjFkYjBmOTBiMzcwNiIsInN1YiI6IjY2NTMyZTIzOWU1ZTFhMjhlNGM2MzFiOCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.1vuZ6E7VvQZie1MYaKOKJwVqh9fKIhy1Wk739Fe7cyU' },
-      });
-      setMovies(response.data.results);
-    } catch (error) {
-      console.error('Error searching movies', error);
+    const form = event.currentTarget;
+    const newQuery = form.elements.query.value.trim();
+    if (newQuery) {
+      setSearchParams({ query: newQuery });
+    } else {
+      setSearchParams({});
     }
   };
 
@@ -25,10 +45,10 @@ const MoviesPage = () => {
       <h1 className={css.title}>Search Movies</h1>
       <form className={css.formStyle} onSubmit={handleSearch}>
         <input
-        className={css.inputStyle}
+          className={css.inputStyle}
           type="text"
-          value={query}
-          onChange={(e) => setQuery(e.target.value)}
+          name="query"
+          defaultValue={query}
         />
         <button className={css.btnSearch} type="submit">Search</button>
       </form>
